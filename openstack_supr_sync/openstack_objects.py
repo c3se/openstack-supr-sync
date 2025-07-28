5419a0533c4b8d3ecd09ae3d30cd5bf6fed1fe87
Leonard Nielsen <leoniel@chalmers.se>
2025-07-28 14:42:23 +0200

    add project-user-role component

--- openstack_supr_sync/openstack_objects.py
+++ openstack_supr_sync/openstack_objects.py
@@ -24,9 +24,12 @@ class OpenstackObjects:
     def get_services(self):
         return self.connection.identity.services()
 
-    def find_user(self, name, user_id=None, filters=None, domain_id=None):
+    def get_user(self, user_id=None):
+        return self.connection.identity.get_user_by_id(user_id)
+
+    def find_user(self, name=None, user_id=None, filters=None, domain_id=None):
         if id is not None:
-            return self.connection.identity.get_user_by_id(user_id)
+            self.get_user(user_id)
         else:
             return self.connection.identity.get_user(name, filters, domain_id)
 
@@ -36,4 +39,29 @@ class OpenstackObjects:
     def create_user(self, name, **kwargs):
         return self.connection.identity.create_user(name, **kwargs)
 
-   # and so on for projects, etc, groups
+    def find_project(self, name=None, project_id=None, filters=None, domain_id=None):
+        if id is not None:
+            return self.get_project(project_id)
+        else:
+            return self.connection.identity.get_project(name)
+
+    def get_project(self, project_id):
+        return self.connection.identity.get_project_by_id(project_id)
+
+    def update_project(self, id, **kwargs):
+        return self.connection.identity.update_project(id, **kwargs)
+
+    def create_project(self, name, **kwargs):
+        """
+        kwargs
+            Description
+
+            enabled
+
+            is_domain
+        """
+        return self.connection.identity.create_project(name, **kwargs)
+
+    def assign_project_role_to_user(self, project, user, role, inherited=False):
+        return self.connection.identity.assign_project_role_to_user(
+            project, user, role, inherited=inherited)
